6d85d19b3645a1b0d2f4794d86dfe5484237c265
owinogabriel <g73346920@gmail.com>
2026-03-29 12:41:08 +0300

    fix:update the correct domain

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || 'http://localhost:3001',
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
   credentials: true
 }))
 
